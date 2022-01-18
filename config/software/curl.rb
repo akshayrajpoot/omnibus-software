f4a043739fe9091653dd27f0d9f0821ad997e554
Akshay Singh <89373241+akshayrajpoot@users.noreply.github.com>
2022-01-18 17:19:01 +0530

    Update curl.rb

--- config/software/curl.rb
+++ config/software/curl.rb
@@ -15,7 +15,7 @@
 #
 
 name "curl"
-default_version "7.80.0"
+default_version "7.81.0"
 
 dependency "zlib"
 dependency "openssl"
@@ -26,6 +26,7 @@ license_file "COPYING"
 skip_transitive_dependency_licensing true
 
 # version_list: url=https://curl.se/download/ filter=*.tar.gz
+version("7.81.0") { source sha256: "1e7a38d7018ec060f1f16df839854f0889e94e122c4cfa5d3a37c2dc56f1e258" }
 version("7.80.0") { source sha256: "dab997c9b08cb4a636a03f2f7f985eaba33279c1c52692430018fae4a4878dc7" }
 version("7.79.1") { source sha256: "370b11201349816287fb0ccc995e420277fbfcaf76206e309b3f60f0eda090c2" }
 version("7.79.0") { source sha256: "aff0c7c4a526d7ecc429d2f96263a85fa73e709877054d593d8af3d136858074" }
